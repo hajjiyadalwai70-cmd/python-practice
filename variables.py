f47df01e937add1cb620c2223723bf6b9bbd21f0
Hajjiya Dalwai <hajjiyadalwai70@gmail.com>
2026-08-12 18:24:00 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,11 @@
+name="Hajjiya"
+age=21
+age2=age
+price=25.99
+print("My name is:",name)
+print("my age is:",age)
+print("my age is:",age2)
+###printing the type of variable
+print(type(name))
+print(type(age))
+print(type(price))
